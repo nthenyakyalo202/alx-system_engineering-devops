2738f6f87455d9a055981d472247cbe2365a859d
nthenyakyalo202 <nthenyakyalo202@gmail.com>
2023-04-24 13:41:07 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,31 @@
+#!/usr/bin/python3
+"""Accessing a REST API for todo lists of employees"""
+
 import requests
 import sys
 
-if len(sys.argv) < 2:
-    print("Please provide an employee ID")
-    sys.exit(1)
 
-employee_id = int(sys.argv[1])
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    response = requests.get(url)
+    employeeName = response.json().get('name')
 
-if response.status_code != 200:
-    print("Error: Unable to retrieve TODO list")
-    sys.exit(1)
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
 
-todos = response.json()
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-completed_tasks = [todo for todo in todos if todo["completed"]]
-total_tasks = len(todos)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
-employee_name = todos[0]["name"].split()[0]
-
-print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-
-for todo in completed_tasks:
-    print(f"\t {todo['title']}")
-
-
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
